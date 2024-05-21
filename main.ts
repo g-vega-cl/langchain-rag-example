90ed40eb31366db3abe0ca4ebb76545fe7ccc5ee
g-vega-cl <g.vega.cl@gmail.com>
2024-05-21 18:07:13 -0400

    tutorial setup successful

---
+++ main.ts
@@ -0,0 +1,51 @@
+import "cheerio";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
+import { pull } from "langchain/hub";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { formatDocumentsAsString } from "langchain/util/document";
+import {
+    RunnableSequence,
+    RunnablePassthrough,
+} from "@langchain/core/runnables";
+
+const loader = new CheerioWebBaseLoader(
+    "https://lilianweng.github.io/posts/2023-06-23-agent/"
+);
+
+const docs = await loader.load();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+});
+
+const splits = await textSplitter.splitDocuments(docs);
+const vectorStore = await MemoryVectorStore.fromDocuments(
+    splits,
+    new OpenAIEmbeddings()
+);
+
+// Retrieve and generate using the relevant snippets of the blog.
+const retriever = vectorStore.asRetriever();
+const prompt = await pull < ChatPromptTemplate > ("rlm/rag-prompt");
+const llm = new ChatOpenAI({ model: "gpt-3.5-turbo", temperature: 0 });
+
+const declarativeRagChain = RunnableSequence.from([
+    {
+        context: retriever.pipe(formatDocumentsAsString),
+        question: new RunnablePassthrough(),
+    },
+    prompt,
+    llm,
+    new StringOutputParser(),
+]);
+
+const answer = await declarativeRagChain.invoke("What is reinforcement learning?");
+
+console.log('----------------------------------------------------------')
+console.log(answer);
